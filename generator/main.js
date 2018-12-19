bf7813aa9e54a941aa67e5b58a07d140ca6b42fe
Fabian Große <saphareas@googlemail.com>
2018-12-19 13:48:38 +0100

    Tabs for indentation
    For customizable indentation preference

--- generator/main.js
+++ generator/main.js
@@ -14,21 +14,21 @@ let relevantArg = process.argv[2];
 
 // Check if the argument is valid, either 'help' or path to a .json file
 if (relevantArg != null && relevantArg != "undefined") {
-  if (relevantArg == "help") {
-    echoHelp();
-  } else { // Try to read the file and continue on success.
-    fs.readFile(relevantArg, (err, data) => {
-      if (err) {
-        console.error(err);
-        console.debug("Argument is not a valid path or file doesn't exist. Use 'help' for usage information.");
-        process.exit(1);
-      }
-      let jsonObj = JSON.parse(data);
-      generateCards(jsonObj);
-    })
-  }
+	if (relevantArg == "help") {
+		echoHelp();
+	} else { // Try to read the file and continue on success.
+		fs.readFile(relevantArg, (err, data) => {
+			if (err) {
+				console.error(err);
+				console.debug("Argument is not a valid path or file doesn't exist. Use 'help' for usage information.");
+				process.exit(1);
+			}
+			let jsonObj = JSON.parse(data);
+			generateCards(jsonObj);
+		})
+	}
 } else {
-  console.debug(" No Argument was given. Use 'help' for usage information.");
+	console.debug(" No Argument was given. Use 'help' for usage information.");
 }
 
 /**
@@ -36,13 +36,13 @@ if (relevantArg != null && relevantArg != "undefined") {
  * @param {object} jsonObj
  */
 function generateCards(jsonObj) {
-  // Build story cards
-  if (jsonObj.story_cards == undefined) {
-    console.debug("Nothing to do for story cards...");
-  } else {
-    console.log("Building your story cards...");
-    buildStoryCards(jsonObj.title, jsonObj.story_cards);
-  }
+	// Build story cards
+	if (jsonObj.story_cards == undefined) {
+		console.debug("Nothing to do for story cards...");
+	} else {
+		console.log("Building your story cards...");
+		buildStoryCards(jsonObj.title, jsonObj.story_cards);
+	}
 }
 
 /**
@@ -51,45 +51,45 @@ function generateCards(jsonObj) {
  * @param {object} story_cards  Object conaining card declarations
  */
 function buildStoryCards(title, story_cards) {
-  fs.readFile("story-template.html", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      console.debug("Something went wrong.");
-      process.exit(1);
-    }
-    const story_template = data;
+	fs.readFile("story-template.html", "utf8", (err, data) => {
+		if (err) {
+			console.error(err);
+			console.debug("Something went wrong.");
+			process.exit(1);
+		}
+		const story_template = data;
 
-    for (i = 0; i < story_cards.length; i+=2) {
-      let j = i;
-      let card = story_template
-        .replace("{{ title }}", title)
-        .replace("{{ index_1 }}", story_cards[j].index)
-        .replace("{{ content_1 }}", story_cards[j].content);
-      j++;
-      if (j < story_cards.length) {
-        card = card
-          .replace("{{ title }}", title)
-          .replace("{{ index_2 }}", story_cards[j].index)
-          .replace("{{ content_2 }}", story_cards[j].content);
-      } else {
-        card = card
-          .replace("{{ title }}", "")
-          .replace("{{ index_2 }}", "")
-          .replace("{{ content_2 }}", "");
-      }
-      (async (card, index) => {
-        const browser = await puppeteer.launch({headless: true});
-        const page = await browser.newPage();
-        await page.setContent(card);
-        await page.pdf({path: `out/card-${index}.pdf`, format: "A4", printBackground: true});
-        await browser.close();
-      })(card, `${--j}-${++j}`);
-    }
-    mergePDFs.merge(process.cwd() + "/out");
-  });
+		for (i = 0; i < story_cards.length; i+=2) {
+			let j = i;
+			let card = story_template
+				.replace("{{ title }}", title)
+				.replace("{{ index_1 }}", story_cards[j].index)
+				.replace("{{ content_1 }}", story_cards[j].content);
+			j++;
+			if (j < story_cards.length) {
+				card = card
+					.replace("{{ title }}", title)
+					.replace("{{ index_2 }}", story_cards[j].index)
+					.replace("{{ content_2 }}", story_cards[j].content);
+			} else {
+				card = card
+					.replace("{{ title }}", "")
+					.replace("{{ index_2 }}", "")
+					.replace("{{ content_2 }}", "");
+			}
+			(async (card, index) => {
+				const browser = await puppeteer.launch({headless: true});
+				const page = await browser.newPage();
+				await page.setContent(card);
+				await page.pdf({path: `out/card-${index}.pdf`, format: "A4", printBackground: true});
+				await browser.close();
+			})(card, `${--j}-${++j}`);
+		}
+		mergePDFs.merge(process.cwd() + "/out");
+	});
 }
 
 function echoHelp() {
-  let helpString = `//TODO: help string`;
-  console.log(helpString);
+	let helpString = `//TODO: help string`;
+	console.log(helpString);
 }
--- generator/mergepdfs.js
+++ generator/mergepdfs.js
@@ -10,21 +10,21 @@ const pdfjs = require("pdfjs");
 const readdir = require("recursive-readdir");
 
 exports.merge = function(dir) {
-  let mergedDoc = new pdfjs.Document();
-  readdir(dir, ["!*.pdf", "merged.pdf"]).then( // get all pdf-files except previous merges
-    function(pdfs) {
-      pdfs.forEach(function(pdf){
-        let file = fs.readFileSync(pdf);
-        let ext = new pdfjs.ExternalDocument(file);
-        mergedDoc.addPagesOf(ext);
-      });
-      mergedDoc.pipe(fs.createWriteStream(dir + '/merged.pdf'));
-      mergedDoc.end().then(function() {
-        console.log("\n Finished writing PDF (" + mergedDoc.pageCount + " pages total).");
-      });
-    },
-    function(error) {
-      throw error;
-    }
-  );
+	let mergedDoc = new pdfjs.Document();
+	readdir(dir, ["!*.pdf", "merged.pdf"]).then( // get all pdf-files except previous merges
+		function(pdfs) {
+			pdfs.forEach(function(pdf){
+				let file = fs.readFileSync(pdf);
+				let ext = new pdfjs.ExternalDocument(file);
+				mergedDoc.addPagesOf(ext);
+			});
+			mergedDoc.pipe(fs.createWriteStream(dir + '/merged.pdf'));
+			mergedDoc.end().then(function() {
+				console.log("\n Finished writing PDF (" + mergedDoc.pageCount + " pages total).");
+			});
+		},
+		function(error) {
+			throw error;
+		}
+	);
 }
